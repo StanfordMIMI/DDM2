597e63584b06e8fddef62c089e6cc5c3cc62e6f1
tiangexiang <tiangex@outlook.com>
2023-02-07 12:23:06 -0800

    update

--- denoise.sh
+++ denoise.sh
@@ -1,8 +1,1 @@
-# python infer_denoise.py -c config/ours_self_denoise_condition.json
-# python infer_denoise.py -c config/ours_v4.json
-# python infer_denoise.py -c config/ours_v6.json
-# python denoise.py -c config/ours_v23.json
-# python denoise.py -c config/ttt.json
-
-# python test.py -c config/ours_v25.json
-python denoise.py -c config/qiyuan.json --save --align_mean
+python3 denoise.py -c config/hardi_150.json --save
--- sample.sh
+++ sample.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-#
-#SBATCH --job-name=get_alpha
-#
-#SBATCH --time=300:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
-#SBATCH -p gpu
-#SBATCH -G 1
-#SBATCH -c 8
-
-
-python sample.py -c config/hardi_150.json
+python3 sample.py -c config/hardi_150.json
