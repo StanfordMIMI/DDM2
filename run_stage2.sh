b97d2edfb3f4c8e92453cefd7cf1ae83230b59f5
tiangexiang <tiangex@outlook.com>
2023-02-07 12:22:04 -0800

    update

--- run_stage2.sh
+++ run_stage2.sh
@@ -1,18 +1,3 @@
-#!/bin/bash
-#
-#SBATCH --job-name=get_alpha
-#
-#SBATCH --time=300:00
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=4G
-#SBATCH -p gpu
-#SBATCH -G 1
-#SBATCH -c 8
-
-#ml reset
-#ml load py-pytorch/1.6.0_py36
-
-# python3 get_alpha.py -p train -c config/hardi_150.json
-python3 match_state.py -p train -c config/qiyuan.json
-# python3 get_alpha.py -p train -c config/ablation_7prior.json
+##### PLEASE UPDTAE CONFIGURE FILE BEFORE EXCUTING THIS SCRIPT #####
+python3 match_state.py -p train -c config/hardi_150.json
+##### PLEASE UPDTAE CONFIGURE FILE BEFORE EXCUTING THIS SCRIPT #####
